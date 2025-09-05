a09ab375d541e74bd527d3c1158a4399afc94374
ARTHUR BARBOSA DORNELAS SILVA <24.00813-3@maua.br>
2025-09-05 11:34:15 -0300

    Aula 05 Multiplataforma

---
+++ Dev Multiplataforma/Aula05-LAB/lista_filmes/test/lista_filmes_test.dart
@@ -0,0 +1,8 @@
+// import 'package:lista_filmes/lista_filmes.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
