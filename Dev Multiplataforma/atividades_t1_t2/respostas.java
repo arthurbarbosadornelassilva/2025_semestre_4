dd0af8aaa7e98e01e923962b984953fbe29c3ce1
arthurbarbosadornelassilva <xarthsilvax@gmail.com>
2025-09-12 10:01:11 -0300

    Atividade T1 multiplataforma

--- Dev Multiplataforma/atividades_t1_t2/respostas.java
+++ Dev Multiplataforma/atividades_t1_t2/respostas.java
@@ -1,11 +1,17 @@
-public class respostas {
-    public static int recur(int x, int y) { 
-    if (x == 0) { 
-        return y; 
-    } 
-        return recur(x - 1, x + y); 
-    } 
-    public static void main (String[]args)    { 
-        System.out.println(recur(4, 3));
-    }
-}
+// public class respostas {
+//     class Animal {
+//     void speak() { System.out.println("Animal sound"); }
+//     }
+// //
+//     class Dog extends Animal {
+//         @Override
+//         void speak() { System.out.println("Bark"); }
+//         void wagTail() { System.out.println("Wagging tail"); }
+//     }
+
+//     public static void main (String [] args) { 
+//         Dog a = new Dog();
+//         a.speak();    
+//         a.wagTail();  
+//     }
+// }
